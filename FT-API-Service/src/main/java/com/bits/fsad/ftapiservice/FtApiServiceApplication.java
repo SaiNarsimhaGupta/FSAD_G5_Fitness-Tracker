1d2013b96be90b20a51a5904e7448b355552a18d
Sai Narsimha Gupta <2023mt93038@wilp.bits-pilani.ac.in>
2024-04-13 18:20:16 +0530

    Created API Service Application

---
+++ FT-API-Service/src/main/java/com/bits/fsad/ftapiservice/FtApiServiceApplication.java
@@ -0,0 +1,14 @@
+package com.bits.fsad.ftapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtApiServiceApplication.class, args);
+        System.out.println("Started FtApiServiceApplication");
+    }
+
+}
